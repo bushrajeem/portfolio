14e3caa05a1046c0441640724a01878087a65f08
bushrajeem <bushrajeem32@gmail.com>
2025-04-07 21:12:32 +0600

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,21 +1,22 @@
-import Layout from "../../layout";
-import Banner from "./components/Banner";
-import Blog from "./components/Blog";
-import Connect from "./components/Connect";
-import { Experience } from "./components/Experience";
-import Services from "./components/Services";
-import Works from "./components/Works";
+// import Layout from "../../layout";
+// import Banner from "./components/Banner";
+// import Blog from "./components/Blog";
+// import Connect from "./components/Connect";
+// import { Experience } from "./components/Experience";
+// import Services from "./components/Services";
+// import Works from "./components/Works";
 
 export const Home =()=>{
   return (
-    <Layout>
-      <Banner />
-      <Services />
-      <Experience />
-      <Works />
-      <Blog />
-      <Connect />
-    </Layout>
+    // <Layout>
+    //   <Banner />
+    //   <Services />
+    //   <Experience />
+    //   <Works />
+    //   <Blog />
+    //   <Connect />
+    // </Layout>
+    <>home</>
   );
 }
 
